292af9b2ca1e94c5fb1401a4c66b2d4a63a90a43
Matt Fisher <mfisher87@gmail.com>
2024-07-02 18:54:35 -0600

    Fix type annotation issues in query module
    Requires an unfortunate use of `cast`; opened a discussion to ask for
    help, as in comments

--- src/aross_stations_db/query.py
+++ src/aross_stations_db/query.py
@@ -1,7 +1,9 @@
 import datetime as dt
+from typing import cast
 
 from sqlalchemy import func
-from sqlalchemy.orm import Query, Session
+from sqlalchemy.orm import Session
+from sqlalchemy.orm.query import RowReturningQuery
 
 from aross_stations_db.tables import Event, Station
 
@@ -12,13 +14,19 @@ def stations_query(
     start: dt.datetime,
     end: dt.datetime,
     polygon: str | None = None,
-) -> Query[Station, float, float, int]:
+) -> RowReturningQuery[tuple[Station, float, float, int]]:
     query = (
-        db.query(
-            Station,
-            Station.location.ST_X(),
-            Station.location.ST_Y(),
-            func.count(),
+        cast(
+            # TODO: A better way! Avoid duplicating information; the type of Station and
+            # func.count() can be inferred automatically, but not ST_X().
+            #       https://github.com/sqlalchemy/sqlalchemy/discussions/11564
+            RowReturningQuery[tuple[Station, float, float, int]],
+            db.query(
+                Station,
+                Station.location.ST_X(),
+                Station.location.ST_Y(),
+                func.count(),
+            ),
         )
         .join(
             Event,
@@ -42,15 +50,21 @@ def timeseries_query(
     start: dt.datetime,
     end: dt.datetime,
     polygon: str | None = None,
-) -> Query[dt.date, int]:
-    query = db.query(
-        func.date_trunc("month", Event.time_start).label("month"),
-        func.count(Event.time_start).label("count"),
+) -> RowReturningQuery[tuple[dt.datetime, int]]:
+    query = cast(
+        # TODO: A better way! Avoid duplicating information; the type of func.count()
+        # can be inferred automatically, but not func.date_trunc().
+        #       https://github.com/sqlalchemy/sqlalchemy/discussions/11564
+        RowReturningQuery[tuple[dt.datetime, int]],
+        db.query(
+            func.date_trunc("month", Event.time_start).label("month"),
+            func.count(Event.time_start).label("count"),
+        ),
     ).filter(Event.time_start >= start, Event.time_end < end)
 
     if polygon:
         query = query.join(
-            Station,
+            Station,  # TODO: Event.station relationship
         ).filter(
             Station.location.ST_Within(
                 func.ST_SetSRID(func.ST_GeomFromText(polygon), 4326),
